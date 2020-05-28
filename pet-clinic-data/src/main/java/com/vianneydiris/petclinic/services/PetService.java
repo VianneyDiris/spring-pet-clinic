3ab600298f66cc1c0a48653632ce72acdb2fa948
vianney diris <vianney.diris@gmail.com>
2020-05-28 19:31:44 +0200

    add Service and Map

--- pet-clinic-data/src/main/java/com/vianneydiris/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/vianneydiris/petclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.vianneydiris.petclinic.services;
 
 import com.vianneydiris.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet,Long>{
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save (Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/vianneydiris/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/vianneydiris/petclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.vianneydiris.petclinic.services;
 
 import com.vianneydiris.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long>{
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save (Vet vet);
-    Set<Vet> findAll();
 }
