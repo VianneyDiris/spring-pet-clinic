c13a91c7a6d9db74736698ad82a0f0b314a0f46c
vianney diris <vianney.diris@gmail.com>
2020-05-29 10:53:06 +0200

    add Vet and Owner web page and controller

---
+++ pet-clinic-web/src/main/java/com/vianneydiris/petclinic/controllers/OwnerController.java
@@ -0,0 +1,13 @@
+package com.vianneydiris.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OwnerController {
+
+    @RequestMapping({"owners","owners/index"})
+    public String listOwner(){
+        return "owners/index";
+    }
+}
