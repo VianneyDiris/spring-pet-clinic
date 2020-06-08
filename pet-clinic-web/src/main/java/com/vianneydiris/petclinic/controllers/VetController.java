9eb3e2b40fcd9d7d31dfe5adef1c80dc4e6319a4
vianney diris <vianney.diris@gmail.com>
2020-06-08 17:15:19 +0200

    add api/vets

--- pet-clinic-web/src/main/java/com/vianneydiris/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/vianneydiris/petclinic/controllers/VetController.java
@@ -1,9 +1,13 @@
 package com.vianneydiris.petclinic.controllers;
 
+import com.vianneydiris.petclinic.model.Vet;
 import com.vianneydiris.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -17,5 +21,10 @@ public class VetController {
     public String listVets(Model model){
         model.addAttribute("vets",vetService.findAll());
         return "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody Set<Vet> getVetsJSON(){
+        return vetService.findAll();
     }
 }
